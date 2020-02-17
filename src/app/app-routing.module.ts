1844bbf57f1328e50243d77ccd3aaf6876e80892
Huldula <38780229+Huldula@users.noreply.github.com>
2020-02-17 21:26:45 +0100

    some olls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {F2lComponent} from './f2l/f2l.component';
+import {OllComponent} from './oll/oll.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/f2l', pathMatch: 'full' },
-  { path: 'f2l', component: F2lComponent }
+  { path: 'f2l', component: F2lComponent },
+  { path: 'oll', component: OllComponent }
 ];
 
 @NgModule({
--- src/app/oll/oll.component.ts
+++ src/app/oll/oll.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OllComponent implements OnInit {
 
+  public number = -1;
+
+  public MIN = 15;
+  public MAX = 28;
+
   constructor() { }
 
   ngOnInit() {
+    this.genNewImg();
+  }
+
+  public genNewImg(): void {
+    let rand = -1;
+    do {
+      rand = Math.floor(Math.random() * (this.MAX + 1 - this.MIN)) + this.MIN;
+    } while (this.number === rand);
+    this.number = rand;
   }
 
 }
